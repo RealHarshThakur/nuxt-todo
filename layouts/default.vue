eeaf6c81ea86b2e7afb8501c57f2c47e642a8ba4
Harsh Thakur <38140305+harshthakur9030@users.noreply.github.com>
2020-04-08 03:04:32 +0530

    Add files via upload

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-app dark>
+
+    <v-app-bar  absolute src="https://picsum.photos/1920/1080?random" color="purple darken-4">    
+      
+        <v-toolbar-title class="mx-auto display-2 font-weight-bold">Todo App</v-toolbar-title> 
+    </v-app-bar>
+  <v-content>
+      <v-container>
+          <nuxt />
+      </v-container>
+  </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+      items: [
+        {
+          to: '/'
+        },
+      ],
+      title: 'Todo'
+    }
+  }
+}
+</script>
+
+<style scoped>
+.text-center{
+  margin: 0 auto;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,98 @@
+<template>
+  <div>
+    <v-container class ="my-12 mx-3">
+      <v-text-field label="Todo" placeholder="List your todo" v-model="task" @keydown.enter="create"></v-text-field>
+  
+        <v-btn @click="create" block icon color="yellow"   >
+          <v-icon left> mdi-plus</v-icon> 
+          <v-span> Todo </v-span> 
+        </v-btn>
+    </v-container>
+
+    <v-divider class="my-12"></v-divider>
+
+    <h2 class="display-3 success--text pl-4">
+        Tasks:&nbsp;
+      <v-fade-transition leave-absolute>
+        <span :key="`tasks-${tasks.length}`">
+          {{ tasks.length }}
+        </span>
+      </v-fade-transition>
+    </h2>
+    
+    <h2 class="display-1 mx-5 my-7 font-weight-medium blue--text">List of todos:</h2>
+
+    <v-container class="my-4 display-1">
+      <v-layout v-for="task in tasks" :key="task.id" row wrap> 
+        <v-flex lg10 md10>
+          <v-card  flat class="  ma-3 pa-2">
+            <span>{{task.text}}</span>
+          </v-card>
+        </v-flex>
+        <v-flex class="my-4 " lg2 md2> 
+          <v-btn dark small color="error"  @click="remove(task.id)" >
+            <v-icon>mdi-minus</v-icon>
+          </v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data: ()=>({
+      tasks: [
+        {
+          id:"1",
+          done: false,
+          text: "This is a really really long text and that is all i had to say to make it long enough\
+          so that I can check something which is irrelevant to this text, anyways the text isn't long enough\
+          yet so I'll be doing this until it is long enough" ,
+        },
+        {
+          id:"2",
+          done: false,
+          text: 'Eat',
+        },
+        {
+          id:"3",
+          done: true,
+          text:'Sleep'
+        },
+        {
+          id:"4",
+          done: true,
+          text:'Sleep again'
+        },
+        {
+          id:"5",
+          done: false,
+          text:'Filling todo list'
+        },
+
+      ],
+      task: null,
+  }),
+
+ methods: {
+      create () {
+        this.tasks.push({
+          done: false,
+          text: this.task,
+        })
+
+        this.task = null
+      },
+    remove(id){
+
+      this.tasks = this.tasks.filter((task)=> task.id!==id);
+
+    }
+  },
+
+
+}
+</script>
