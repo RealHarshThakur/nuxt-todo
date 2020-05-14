0c2b6e4f716b5b1ee10387b98954652c97f4b541
Harsh Thakur <38140305+harshthakur9030@users.noreply.github.com>
2020-05-14 09:06:59 +0000

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,6 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
     title: "Todo App",
     meta: [
       { charset: 'utf-8' },
